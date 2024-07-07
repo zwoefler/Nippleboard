6557cd90d90f3037e0b6b6c279fc8b0cd47bc756
Martin <martinfz@t-online.de>
2024-07-07 17:20:55 +0200

    Add play and pause function + visuals

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,52 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="sound in sounds" :key="sound.name">
-        <button @click="playSound(sound.url)">{{ sound.name }}</button>
-      </li>
-    </ul>
-  </div>
+  <ul class="grid grid-cols-3 md:grid-cols-6 gap-4 p-4">
+    <li v-for="sound in sounds" :key="sound.name">
+      <div class="h-32 max-w-32 bg-gray-300 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button @click="togglePlay(sound)"
+          class="w-12 h-12 flex flex-row items-center gap-2 justify-center bg-blue-500 rounded-full shadow-lg hover:cursor-pointer hover:drop-shadow-xl hover:bg-blue-600 hover:scale-105">
+          <svg v-if="sound.isPlaying" id="play-button" stroke="currentColor" fill="currentColor" stroke-width="0"
+            viewBox="0 0 448 512" class="text-slate-200 hover:scale-125" height="1em" width="1em">
+            <path
+              d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z">
+            </path>
+          </svg>
+          <svg v-else id="pause-button" stroke="currentColor" fill="currentColor" viewBox="0 0 32 32"
+            enable-background="new 0 0 32 32">
+            <g>
+              <path fill="currentColor"
+                d="M19.609,14.802L14.316,10.8c-0.263-0.197-0.567-0.302-0.88-0.302C12.631,10.498,12,11.158,12,12v8 c0,0.842,0.635,1.502,1.445,1.502c0.313,0,0.619-0.104,0.885-0.304l5.258-3.998c0.382-0.287,0.611-0.742,0.611-1.218 C20.198,15.512,19.983,15.081,19.609,14.802z M18.985,16.401l-5.258,3.998C13.434,20.622,13,20.453,13,20v-8 c0-0.326,0.225-0.502,0.437-0.502c0.096,0,0.19,0.034,0.279,0.101l5.293,4.002c0.165,0.123,0.19,0.292,0.19,0.382 C19.198,16.147,19.122,16.3,18.985,16.401z">
+              </path>
+            </g>
+          </svg>
+        </button>
+        <p class="text-xs">
+          {{ sound.name }}
+        </p>
+      </div>
+    </li>
+
+  </ul>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 
 const sounds = ref([]);
+
+function togglePlay(sound) {
+  if (!sound.audio) {
+    sound.audio = new Audio(sound.url);
+    sound.audio.addEventListener('ended', () => {
+      sound.isPlaying = false;
+    });
+  }
+  if (sound.isPlaying) {
+    sound.audio.pause();
+  } else {
+    sound.audio.play();
+  }
+  sound.isPlaying = !sound.isPlaying;
+}
 
 onMounted(async () => {
   const soundFiles = import.meta.glob('../assets/sounds/*.mp3', { eager: true });
@@ -21,8 +56,4 @@ onMounted(async () => {
   }));
 });
 
-function playSound(url) {
-  const audio = new Audio(url);
-  audio.play();
-}
 </script>
