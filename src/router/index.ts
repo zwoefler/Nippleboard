4d2ac65c07e09b620a93691649623dcf5b2e6740
zwoefler <>
2024-07-15 18:38:44 +0200

    Delete unnecessary upload route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,18 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import SoundDetail from '../views/SoundDetail.vue';
 import Login from '../views/Login.vue';
-import Upload from '../views/Upload.vue';
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home
-    },
-    {
-        path: '/upload',
-        name: 'Upload',
-        component: Upload
     },
     {
         path: '/sound/:id',
