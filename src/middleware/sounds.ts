7ea81aa7d7ece7f1ffff69d4a4d861cf7d148194
zwoefler <>
2024-07-29 19:16:58 +0200

    Small aufräuming in sounds.ts middleware

--- src/middleware/sounds.ts
+++ src/middleware/sounds.ts
@@ -7,12 +7,12 @@ export async function fetchSoundsMetadata() {
 
         const soundPaths = soundsMetadata.map(item => item.bucket_item);
         const soundURLs = await fetchSoundsURLs(soundPaths);
-        const mergedData = soundsMetadata.map(item => {
+        const mergedSoundsMetadata = soundsMetadata.map(item => {
             const match = soundURLs.find(urlItem => urlItem.path === item.bucket_item);
             return match ? { ...item, signedURL: match.signedUrl } : item;
         });
-        console.log("MERGED DATA", mergedData)
-        // return mergedData;
+
+        return mergedSoundsMetadata;
     } catch (error) {
         console.error('Error fetching or merging sound metadata:', error);
         return [];
