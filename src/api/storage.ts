5f61bd3a7d090fe9bcd41cdcdaae416f83089c4f
zwoefler <>
2024-08-04 11:59:10 +0200

    Delete unnecessary fetchSoundFromSupabase function

--- src/api/storage.ts
+++ src/api/storage.ts
@@ -26,43 +26,6 @@ export async function fetchSoundsURLs(soundPaths: []) {
     }
 }
 
-export async function fetchSoundsFromSupabase(): Promise<Sound[]> {
-    try {
-        const soundFolder = "sounds/"
-        const { data: soundsList } = await supabase
-            .storage
-            .from('sounds')
-            .list('sounds', {
-                limit: 100
-            });
-
-        if (!soundsList) {
-            console.error('No sounds list found.');
-            return [];
-        }
-
-        const soundPaths = soundsList.map(file => soundFolder + file.name);
-        const { data: signedURLs, error: urlsError } = await supabase
-            .storage
-            .from('sounds')
-            .createSignedUrls(soundPaths, 3600);
-
-        if (urlsError) {
-            console.error('Error getting signed URLs:', urlsError);
-            return [];
-        }
-
-        const sounds = signedURLs.map(({ path, signedUrl }) => ({
-            name: path?.split('/').pop() || "Unnamed Sound",
-            url: signedUrl
-        }));
-        return sounds
-    } catch (error) {
-        console.error('Error fetching sounds from Supabase:', error);
-        return []
-    }
-}
-
 export async function downloadSoundFromSupabase(soundName: string) {
     try {
         const { data, error } = await supabase.storage.from('sounds').download(`sounds/${soundName}`);
