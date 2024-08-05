7bfc8433c2a1465a5cc87e9be95adbdf69f191b8
zwoefler <>
2024-08-05 13:29:57 +0200

    Fix emitting Inputs

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,3 +1,16 @@
 <template>
-  <input type="text" class="text-gray-700 p-2 border rounded w-full pl-3 pr-10">
+  <input type="text" :value="modelValue" @input="updateValue" @change="onChange"
+    class="text-gray-700 p-2 border rounded w-full pl-3 pr-10">
 </template>
+
+<script setup>
+const props = defineProps({
+  modelValue: String
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+const updateValue = (event) => {
+  emit('update:modelValue', event.target.value)
+}
+</script>
